a503f6166f2ab99ce471f74a2d85c7d12a8fb21d
bingtimren <bingtimren@gmail.com>
2015-09-01 07:58:47 +1000

    w

--- src/format.c
+++ src/format.c
@@ -3,5 +3,5 @@
 
 /* convert time to localtime and format to "HH:MM" */
 void fmt_time_24h(char* buffer, const size_t size, const time_t *time_ptr) {
-  strftime(buffer, size, "%H:%M", localtime(time_ptr));
+  strftime(buffer, size, FORMAT_24HTIME, localtime(time_ptr));
 }
--- src/format.h
+++ src/format.h
@@ -1,4 +1,7 @@
 #pragma once
   
+#define FORMAT_24HTIME "%H:%M %S"
+#define FORMAT_24HTIME_BUFFER_LENGTH 9
+  
 /* convert time to localtime and format to "HH:MM" */
 void fmt_time_24h(char* buffer, const size_t size, const time_t *time_ptr);

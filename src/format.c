e9bd78ff8ae6a8a924a62245fd87ed4c2a229519
Bing Ren <bing.ren@eastip.com>
2015-10-13 00:07:18 +1100

    ii\

--- src/format.c
+++ src/format.c
@@ -14,11 +14,7 @@ void fmt_time_24h(char* buffer, const size_t size, const time_t *time_ptr) {
 void fmt_to_timediff(struct TimeDifference* result_buffer, time_t t1, time_t t2) {
   (*result_buffer).earlier = (t1 < t2);
   uint32_t secdiff = abs(t1 - t2);
-#ifdef DEBUG_TO_SECONDS
   (*result_buffer).seconds = secdiff % SEC_IN_MINUTE;
-#else
-  (*result_buffer).seconds = 0;
-#endif
   (*result_buffer).minutes =  (secdiff % SEC_IN_HOUR) / SEC_IN_MINUTE;
   (*result_buffer).hours = (secdiff % SEC_IN_DAY) / SEC_IN_HOUR;
   (*result_buffer).days = secdiff / SEC_IN_DAY;
@@ -29,9 +25,11 @@ bool fmt_timediff_str(char* buffer, size_t size, time_t t1, time_t t2) {
   struct TimeDifference tdiff;
   fmt_to_timediff(&tdiff, t1, t2);
   if (tdiff.days != 0) {
-    snprintf(buffer, size, "%ud %uh %um", tdiff.days, tdiff.hours, tdiff.minutes);
+    snprintf(buffer, size, "%ud %uh", tdiff.days, tdiff.hours);
+  } else if (tdiff.hours != 0) {
+    snprintf(buffer, size, "%uh %um", tdiff.hours, tdiff.minutes);
   } else {
-    snprintf(buffer, size, "%uh %um %us", tdiff.hours, tdiff.minutes, tdiff.seconds);
+    snprintf(buffer, size, "%um %us", tdiff.minutes, tdiff.seconds);
   };
   return tdiff.earlier;
 }

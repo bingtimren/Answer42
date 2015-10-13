c45972bc68fb842f51f41bc7f32c5d4e5f267cb1
Bing Ren <bing.ren@eastip.com>
2015-10-14 00:48:41 +1100

    added some missing files

---
+++ src/debug_debug.h
@@ -0,0 +1,22 @@
+// when setting seconds per minute to 1, makes debugging easier cause time lapses faster
+
+#define DEBUG_TO_SECONDS
+#define DEBUG_CHECK_MORE
+#define DEBUG_SAVE_DEBUG_RECORDS
+#define VERSION_INFO "Trudi"
+
+/* Testings
+ * 
+ * 1. run Nothing and see if reminders triggered 5x2_5x10_30
+ * 2. select Work and see if reminded 15x2_15 and repeats forever (see log repeating)
+ * 3. commit and not-confirm, should return to work
+ * 4. commit and confirm, without select, should return to nothing
+ * 5. commit to show selection screen, select Hygine, shall commit as is after 30_10x5
+ * 6. should show nothing after auto commit
+ * 7. select what_domestic, should stay no-reminder after 30_10x5
+ * 8. test abandon current session - confirm / no-confirm
+ * 9. test communication - before each commit
+ * 10 test reset 
+ * 11 select something - exit - get back, see things still there and data still there
+ * 12 exit - wait for reminder to kick out and return to screen
+ * */
---
+++ src/reminder_stages.h
@@ -0,0 +1,49 @@
+/*********************************************************************************************/
+/*                                 Reminder stages                                           */
+/*********************************************************************************************/
+  
+const struct WhatReminderStageType r5x2_5x10_30[] = {
+  {5,2},
+  {5,10},
+  {30,0}
+};
+
+const struct WhatReminderStageType r15x2_15[] = {
+  {15,2},
+  {15,0}
+};
+
+const struct WhatReminderStageType r480_30x4[] = {
+  {480,1},
+  {30,4}
+};
+
+const struct WhatReminderStageType r60_60x3[] = {
+  {60,1},
+  {60,3}
+};
+
+const struct WhatReminderStageType r30_10x5[] = {
+  {30,1},
+  {10,5}
+};
+
+const struct WhatReminderStageType r30_30[] = {
+  {30,1},
+  {30,0}
+};
+
+const struct WhatReminderStageType r180_60x5[] = {
+  {180,1},
+  {60,5}
+};
+
+const struct WhatReminderStageType r30x2_15x4[] = {
+  {30,2},
+  {15,4}
+};
+
+const struct WhatReminderStageType r5_5x3[] = {
+  {5,1},
+  {5,3}
+};

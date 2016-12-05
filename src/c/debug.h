f1661a3c33ab515fa884c67b4a6deab118eabde5
Bing Ren <bing.ren@eastip.com>
2016-12-05 13:12:07 +1100

    Version 2.1
    New interface / local summary tested
    "No conn" bug fixed (changed src/pkjs to src/js and disabled multiple js module)

--- src/c/debug.h
+++ src/c/debug.h
@@ -1,8 +1,8 @@
-#include "debug_debug.h"
-// #include "debug_production.h"
+// #include "debug_debug.h"
+#include "debug_production.h"
 
 // the debug switch, just include either "debug_debug.h"
 // or the "debug_production.h"
 
-#define VERSION_INFO "2.0 (Joanna)"
+#define VERSION_INFO "2.1 (Joanna)"
 
